e62c45a02bd7e7ce50b6e593e91ec61cc9baea39
graveknave <hunter.e.canavan@gmail.com>
2021-06-15 20:01:34 -0400

    Leave a Review css updated

--- src/Components/BookDetail.js
+++ src/Components/BookDetail.js
@@ -72,97 +72,99 @@ const BookDetail = ({ books, match, bookDetail, setBookDetails }) => {
 	};
 
 	return (
-		<section>
-			{/* <button className='open-modal'>Leave a Review</button> */}
-			{!bookDetail ? null : (
-				<aside className='book-detail'>
-					<h1>{bookDetail.title}</h1>
-					<h3>{bookDetail.author}</h3>
-					<img src={bookDetail.book_image} alt={bookDetail.title} />
-					<p>{bookDetail.description}</p>
-					<a
-						href={bookDetail.amazon_product_url}
-						target='_blank'
-						rel='noopener noreferrer'>
-						Buy on Amazon
-					</a>
-				</aside>
-			)}
-			<h2>Leave a Review</h2>
-			<container className='leave-review'>
-				<form onSubmit={handleSubmit} className='rating-form'>
-					{errText ? (
-						<p>Please leave a rating and review before submitting</p>
-					) : null}
-					{/* <p>Rating:</p> */}
-					<wrapper className='rate'>
-						<input
-							type='radio'
-							id='star5'
-							name='rating'
-							value='5'
-							onChange={handleChange}
-						/>
-						<label title='text'>5 stars</label>
-						<input
-							type='radio'
-							id='star4'
-							name='rating'
-							value='4'
-							onChange={handleChange}
-						/>
-						<label title='text'>4 stars</label>
-						<input
-							type='radio'
-							id='star3'
-							name='rating'
-							value='3'
-							onChange={handleChange}
-						/>
-						<label title='text'>3 stars</label>
-						<input
-							type='radio'
-							id='star2'
-							name='rating'
-							value='2'
-							onChange={handleChange}
-						/>
-						<label title='text'>2 stars</label>
-						<input
-							type='radio'
-							id='star1'
-							name='rating'
-							value='1'
-							onChange={handleChange}
-						/>
-						<label title='text'>1 star</label>
-					</wrapper>
-					<div className='review'>
-						{/* <p>Your review:</p> */}
-						<input
-							name='review'
-							type='text'
-							className='review-text'
-							value={bookObj.review}
-							onChange={handleChange}
-						/>
-						<button type='submit'>Submit</button>
-					</div>
-				</form>
-			</container>
-			<div className='Average'>
-				<h4>Average Rating</h4>
-				{!ratings.length ? <p>Rate me!</p> : <p>{avgRating}</p>}
-			</div>
-			<div className='pastReviews'>
-				<h4>Reviews</h4>
-				{!reviews.length ? (
-					<p>Please leave a review</p>
-				) : (
-					reviews.map((review, i) => <p key={i}>{review}</p>)
+		<>
+			<section>
+				{/* <button className='open-modal'>Leave a Review</button> */}
+				{!bookDetail ? null : (
+					<aside className='book-detail'>
+						<h1>{bookDetail.title}</h1>
+						<h3>{bookDetail.author}</h3>
+						<img src={bookDetail.book_image} alt={bookDetail.title} />
+						<p>{bookDetail.description}</p>
+						<a
+							href={bookDetail.amazon_product_url}
+							target='_blank'
+							rel='noopener noreferrer'>
+							Buy on Amazon
+						</a>
+					</aside>
 				)}
-			</div>
-		</section>
+				<container className='leave-review'>
+					<div className='Average'>
+						<h4>Average Rating</h4>
+						{!ratings.length ? <p>Rate me!</p> : <p>{avgRating}</p>}
+					</div>
+					<h2>Leave a Review</h2>
+					<form onSubmit={handleSubmit} className='rating-form'>
+						{errText ? (
+							<p>Please leave a rating and review before submitting</p>
+						) : null}
+						{/* <p>Rating:</p> */}
+						<wrapper className='stars'>
+							<input
+								type='radio'
+								id='star5'
+								name='rating'
+								value='5'
+								onChange={handleChange}
+							/>
+							<label title='text'>5 stars</label>
+							<input
+								type='radio'
+								id='star4'
+								name='rating'
+								value='4'
+								onChange={handleChange}
+							/>
+							<label title='text'>4 stars</label>
+							<input
+								type='radio'
+								id='star3'
+								name='rating'
+								value='3'
+								onChange={handleChange}
+							/>
+							<label title='text'>3 stars</label>
+							<input
+								type='radio'
+								id='star2'
+								name='rating'
+								value='2'
+								onChange={handleChange}
+							/>
+							<label title='text'>2 stars</label>
+							<input
+								type='radio'
+								id='star1'
+								name='rating'
+								value='1'
+								onChange={handleChange}
+							/>
+							<label title='text'>1 star</label>
+						</wrapper>
+						<div className='review'>
+							{/* <p>Your review:</p> */}
+							<textarea
+								name='review'
+								className='review-text'
+								value={bookObj.review}
+								onChange={handleChange}
+							/>
+							<br />
+							<button type='submit'>Submit</button>
+						</div>
+					</form>
+				</container>
+				<div className='pastReviews'>
+					<h4>Reviews</h4>
+					{!reviews.length ? (
+						<p>Please leave a review</p>
+					) : (
+						reviews.map((review, i) => <p key={i}>{review}</p>)
+					)}
+				</div>
+			</section>
+		</>
 	);
 };
 
